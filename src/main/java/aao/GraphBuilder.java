1b092fc4fde60087bf0fbb30a4ccb7bf65604056
DIOGO ALVES MACHADO NETO <8200435@estg.ipp.pt>
2022-07-06 19:11:36 +0100

    fromCSV to integer matrix

--- src/main/java/aao/GraphBuilder.java
+++ src/main/java/aao/GraphBuilder.java
@@ -4,7 +4,10 @@ import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileReader;
 import java.net.URL;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
 import java.util.Arrays;
+import java.util.stream.Stream;
 
 public class GraphBuilder {
     private static final String COMMA_DELIMITER = ",";
@@ -19,8 +22,15 @@ public class GraphBuilder {
         FileReader reader = new FileReader(file);
         BufferedReader br = new BufferedReader(reader);
 
+        int lineCount;
+        try (Stream<String> stream = Files.lines(file.toPath(), StandardCharsets.UTF_8)) {
+            lineCount = (int) stream.count();
+        }
+
+        int[][] matrix = new int[lineCount][3];
 
         String line;
+        int index = 0;
         while ((line = br.readLine()) != null) {
             String[] values = line.split(COMMA_DELIMITER);
             System.out.println(Arrays.toString(values));
@@ -29,11 +39,15 @@ public class GraphBuilder {
                 throw new IllegalStateException(path + " (Has more or less then 4 columns! Accepted columns are: arc, u, v, cost)");
             }
 
-            int u = Integer.parseInt(values[0]);
-            int v = Integer.parseInt(values[1]);
-            int cost = Integer.parseInt(values[2]);
-        }
+            matrix[index][0] = Integer.parseInt(values[0]);
+            matrix[index][1] = Integer.parseInt(values[1]);
+            matrix[index][2] = Integer.parseInt(values[2]);
 
-        return new int[][]{};
+            index++;
+        }
+        System.out.println("----------- MATRIX ------------");
+        System.out.println(Arrays.deepToString(matrix));
+        System.out.println("-------------------------------");
+        return matrix;
     }
 }
